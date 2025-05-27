83f7dd2ded03f71871ee0df936eb544068868b1c
Arwa Ahmed <arwamozy@gmail.com>
2025-05-27 12:22:11 +0300

    Add all calculator project files

---
+++ bin/calculator1.dart
@@ -0,0 +1,5 @@
+import 'package:calculator1/calculator1.dart' as calculator1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculator1.calculate()}!');
+}
---
+++ test/calculator1_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculator1/calculator1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
